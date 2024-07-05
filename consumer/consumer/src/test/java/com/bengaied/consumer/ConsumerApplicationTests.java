e8a4b9ec90680753142f814201c5e1be465c315c
Ahmed <106811601+ahmed20450@users.noreply.github.com>
2024-07-05 13:16:03 +0200

    Initial commit

---
+++ consumer/consumer/src/test/java/com/bengaied/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bengaied.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
